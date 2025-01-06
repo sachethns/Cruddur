9381b72f3875be3a46a0eb9bede4c9b010944eea
Sacheth Nagamangala Srikanth <sachethns@gmail.com>
2025-01-05 22:43:30 -0500

    Add files via upload

---
+++ frontend-react-js/src/components/MessageForm.js
@@ -0,0 +1,67 @@
+import './MessageForm.css';
+import React from "react";
+import process from 'process';
+import { useParams } from 'react-router-dom';
+import {post} from '../lib/Requests';
+import FormErrors from 'components/FormErrors'
+
+export default function ActivityForm(props) {
+  const [count, setCount] = React.useState(0);
+  const [message, setMessage] = React.useState('');
+  const params = useParams();
+  const [errors, setErrors] = React.useState([]);
+
+  const classes = []
+  classes.push('count')
+  if (1024-count < 0){
+    classes.push('err')
+  }
+
+  const onsubmit = async (event) => {
+    event.preventDefault();
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/messages`
+    let payload_data = {'message': message}
+    if (params.handle) {
+      payload_data.handle = params.handle
+    } else {
+      payload_data.message_group_uuid = params.message_group_uuid
+    }
+    post(url,payload_data, {
+      auth: true,
+      setErrors: setErrors,
+      success: function(){
+        console.log('payload_data:',payload_data)
+        if (payload_data.message_group_uuid) {
+          console.log('redirect to message group')
+          window.location.href = `/messages/${payload_data.message_group_uuid}`
+        } else {
+          props.setMessages(current => [...current,payload_data]);
+        }
+        }
+    })
+  }
+
+  const textarea_onchange = (event) => {
+    setCount(event.target.value.length);
+    setMessage(event.target.value);
+  }
+
+  return (
+    <form 
+      className='message_form'
+      onSubmit={onsubmit}
+    >
+      <textarea
+        type="text"
+        placeholder="send a direct message..."
+        value={message}
+        onChange={textarea_onchange} 
+      />
+      <div className='submit'>
+        <div className={classes.join(' ')}>{1024-count}</div>
+        <button type='submit'>Message</button>
+      </div>
+      <FormErrors errors={errors} />
+    </form>
+  );
+}
---
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -0,0 +1,64 @@
+import './ProfileInfo.css';
+import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
+import ProfileAvatar from 'components/ProfileAvatar';
+import React from "react";
+
+import { Auth } from 'aws-amplify';
+
+
+export default function ProfileInfo(props) {
+  const [popped, setPopped] = React.useState(false);
+
+  const click_pop = (event) => {
+    setPopped(!popped)
+  }
+
+  const signOut = async () => {
+    try {
+        await Auth.signOut({ global: true }); //forces all sessions to close
+        window.location.href = "/"
+        // removing access Token from localStorage when signing out
+        localStorage.removeItem("access_token")
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+  }
+
+  /* Original code utilizing cookies:
+  const signOut = async () => {
+    console.log('signOut')
+    // [TODO] Authenication
+    Cookies.remove('user.logged_in')
+    //Cookies.remove('user.name')
+    //Cookies.remove('user.username')
+    //Cookies.remove('user.email')
+    //Cookies.remove('user.password')
+    //Cookies.remove('user.confirmation_code')
+    window.location.href = "/"
+  }
+  */
+
+  const classes = () => {
+    let classes = ["profile-info-wrapper"];
+    if (popped === true){
+      classes.push('popped');
+    }
+    return classes.join(' ');
+  }
+
+  return (
+    <div className={classes()}>
+      <div className="profile-dialog">
+        <button onClick={signOut}>Sign Out</button> 
+      </div>
+      <div className="profile-info" onClick={click_pop}>
+        <ProfileAvatar id={props.user.cognito_user_uuid} />
+        <div className="profile-desc">
+          <div className="profile-display-name">{props.user.display_name || "My Name" }</div>
+          <div className="profile-username">@{props.user.handle || "handle"}</div>
+        </div>
+        <ElipsesIcon className='icon' />
+      </div>
+    </div>
+  )
+}
